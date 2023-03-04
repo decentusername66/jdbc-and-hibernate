ae300ef60f6f82f3dad55d157bd9637e960265b3
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-04 14:46:53 +0100

    [add] - using @FunctionalInterface and describing its purpose 1. @FunctionalInterface annotation 2. Default methods in interfaces

--- src/main/java/pl/sda/orange2/lambda/BoysDoCry.java
+++ src/main/java/pl/sda/orange2/lambda/BoysDoCry.java
@@ -1,6 +1,9 @@
 package pl.sda.orange2.lambda;
 
+@FunctionalInterface
 public interface BoysDoCry {
     //funkcyjny iterface - jedna abstrakcyjna metoda
     void silnoreki();
+
+    default void cry(){};
 }
