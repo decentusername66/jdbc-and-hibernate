64c31932abc0300437ea53fd761b3fc0977ebef7
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-04 13:49:57 +0100

    [add] - anonymous exercises examples

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -4,5 +4,17 @@ public class LambdaExercises {
     public static void main(String[] args) {
         BoysDoCry boy = new BoysCryToo();
         boy.silnoreki();
+
+        BoysCryToo boy2 = new BoysCryToo();
+        boy2.sing();
+
+        //anonymous class
+        BoysDoCry anonymousClassReference = new BoysDoCry() {
+            @Override
+            public void silnoreki() {
+                System.out.println("Anonimowy silnoręki");
+            }
+        };
+        anonymousClassReference.silnoreki();
     }
 }
