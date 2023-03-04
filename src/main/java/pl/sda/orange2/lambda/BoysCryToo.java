48d50f5aab4122a6db6bea18df0a9433eed5a35d
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-04 13:35:07 +0100

    [add] - polymorphism

---
+++ src/main/java/pl/sda/orange2/lambda/BoysCryToo.java
@@ -0,0 +1,12 @@
+package pl.sda.orange2.lambda;
+
+public class BoysCryToo implements BoysDoCry{
+    @Override
+    public void silnoreki() {
+        System.out.println("Cry, cry, cry...");
+    }
+
+    public void sing() {
+        System.out.println("La, la, la...");
+    }
+}
--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -2,6 +2,7 @@ package pl.sda.orange2.lambda;
 
 public class LambdaExercises {
     public static void main(String[] args) {
-
+        BoysDoCry boy = new BoysCryToo();
+        boy.silnoreki();
     }
 }
