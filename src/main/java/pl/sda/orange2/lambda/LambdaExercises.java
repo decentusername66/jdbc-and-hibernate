aae3f349ccba22186e77a75cd1e12bd70cf8d859
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-04 15:21:40 +0100

    [add] - lambda with input parameters and returned value

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -41,5 +41,7 @@ public class LambdaExercises {
             return "my coffee";
         };
         CoffeeMaker shortVersion = (water, coffeeType) -> "my coffee";
+        shortVersion.prepare(5,"Large latte");
+
     }
 }
