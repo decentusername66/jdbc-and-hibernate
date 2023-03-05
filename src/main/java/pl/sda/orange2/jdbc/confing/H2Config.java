33f265a7425d591852bd07728d83f415fb5825bc
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-05 14:50:42 +0100

    [add] - connection parameter

---
+++ src/main/java/pl/sda/orange2/jdbc/confing/H2Config.java
@@ -0,0 +1,7 @@
+package pl.sda.orange2.jdbc.confing;
+
+public class H2Config {
+    public final static String DB_URL = "";
+    public final static String USER = "";
+    public final static String PASSWORD = "";
+}
