e09490b3d725a4c7342cd0b284fb3311f76905af
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-05 12:12:34 +0100

    [add] - documentation update - stupid one

--- src/main/java/pl/sda/orange2/lambda/BoysCryToo.java
+++ src/main/java/pl/sda/orange2/lambda/BoysCryToo.java
@@ -1,6 +1,6 @@
 package pl.sda.orange2.lambda;
 
-public class BoysCryToo implements BoysDoCry{
+public class BoysCryToo implements BoysDoCry {
     @Override
     public void silnoreki() {
         System.out.println("Cry, cry, cry...");
