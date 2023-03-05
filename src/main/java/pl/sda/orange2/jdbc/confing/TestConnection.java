718b0e27b2da20cb889773e41e69726d5118155e
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-05 15:59:27 +0100

    [add] - connection parameter

--- src/main/java/pl/sda/orange2/jdbc/confing/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/confing/TestConnection.java
@@ -1,17 +1,34 @@
 package pl.sda.orange2.jdbc.confing;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestConnection {
     public static void main(String[] args) {
-
+        String query = """
+                SELECT ID, NAME
+                FROM TEST
+                """;
+        try {
+//            Connection h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+//                    H2Config.USER,
+//                    H2Config.PASSWORD); - to samo
             var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
             System.out.println("got connection: " + (h2Connection != null));
-        } catch (SQLException exc) {
+
+            Statement queryStatement = h2Connection.createStatement();
+            ResultSet queryResult = queryStatement.executeQuery(query);
+            while (queryResult.next()) {
+                System.out.println("id: " + queryResult.getInt(1));
+                System.out.println("name: " + queryResult.getString(2));
+            }
+        }catch (SQLException exc) {
             System.out.println("got exception: " + exc);
+            exc.printStackTrace();
         }
     }
 }
